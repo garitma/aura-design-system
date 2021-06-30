0290d4e4095332137cd6947fb180236c29595081
Pablo Orozco Montes <me@pablopvsky.com>
2021-06-29 20:52:25 -0500

    Create accordion component

---
+++ molecules/accordion.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Button from "../atoms/button";
+import Icon from "../atoms/Icon";
+import { SharedBasic } from "../types/global";
+
+export interface AccordionProps extends SharedBasic {
+    title: string
+}
+const Accordion = ({ title, children } : AccordionProps)  => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="hide-medium hide-large">
+      <div className="aura anchor">
+        <span className="h5">{title}</span>
+        <div className="pin right top bottom valign">
+          <Button mode="link" onClick={() => setIsOpen(!isOpen)}>
+            <Icon sprite={isOpen ? "arrowUp" : "arrowDown"} />
+          </Button>
+        </div>
+      </div>
+      {isOpen && <ul>{children}</ul>}
+    </div>
+  );
+};
+
+export default Accordion;
