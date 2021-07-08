e04fcdddb45467e51540076abfcd1088a20a28be
Pablo Orozco Montes <me@pablopvsky.com>
2021-07-07 22:52:10 -0500

    Fix accordion bug

--- molecules/accordion.tsx
+++ molecules/accordion.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import Button from "../atoms/button";
-import Icon from "../atoms/Icon";
-import { SharedBasic } from "../types/global";
+import Icon from "../atoms/icon";
+import { SharedBasic, AuraHeadline } from "../types/global";
 
 export interface AccordionProps extends SharedBasic {
     title: string
+    headline: AuraHeadline
 }
-const Accordion = ({ title, children } : AccordionProps)  => {
+const Accordion = ({ title, children, headline = "h6" } : AccordionProps)  => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <div className="aura anchor" onClick={() => setIsOpen(!isOpen)}>
-        <span className="h5">{title}</span>
+      <button className="aura anchor" onClick={() => setIsOpen(!isOpen)}>
+        <span className={headline}>{title}</span>
         <div className="pin right top bottom valign">
           <Button mode="link" onClick={() => setIsOpen(!isOpen)}>
             <Icon sprite={isOpen ? "arrowUp" : "arrowDown"} className="t03"/>
           </Button>
         </div>
-      </div>
+      </button>
       {isOpen && <div>{children}</div>}
     </div>
   );
