73d3c72d33b05477855c7459d937db1bf9fc9fad
Pablo Orozco Montes <pablopvsky@pablos-air.lan>
2022-11-27 23:00:32 -0500

    Remove switch teme

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,3 +1,13 @@
 export default {
   outDir: "ladle-build/dist",
+  addons: {
+    theme: {
+      enabled: false,
+      defaultState: "light",
+    },
+    rtl: {
+      enabled: false,
+      defaultState: true,
+    }
+  },
 };
