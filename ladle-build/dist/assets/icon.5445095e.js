5e554855739ed2c125533ade2383c22a9ab5f7fb
Pablo Orozco <me@pablopvsky.com>
2022-11-29 09:13:51 -0500

    Really udatre

--- ladle-build/dist/assets/icon.5445095e.js
+++
@@ -1,1 +0,0 @@
-import{j as c}from"./index.193577f5.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
---
+++ ladle-build/dist/assets/icon.da237bbd.js
@@ -0,0 +1,1 @@
+import{j as c}from"./index.38719538.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
