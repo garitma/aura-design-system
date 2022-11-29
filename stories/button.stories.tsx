f18f2193d62dfb94de1752b18c570a53cf1c6838
Pablo Orozco Montes <pablopvsky@pablos-air.lan>
2022-11-28 20:55:12 -0500

    Add section component

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -5,7 +5,7 @@ export const ButtonFill = () => <Button label="Button fill" />;
 export const ButtonPill = () => <Button mode="pill" label="Button Pill" />;
 export const ButtonLink = () => <Button mode="link" label="Button Pill" />;
 export const ButtonFluid = () => <Button isFluid label="Button Fluid" />;
-export const ButtonDisabled = () => <Button isFluid label="Disabled" isDisabled/>;
+export const ButtonDisabled = () => <Button label="Disabled" isDisabled/>;
 export const ButtonWaiting = () => (
   <Button isWaiting isWaitingText="Loading..." label="Button Fluid"></Button>
 );
---
+++ stories/section.stories.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Section from "../layout/section";
+
+export const Default = () => (
+  <Section>
+    Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos. Posología:
+    Aplicar un abrazo osado con un apretón fuerte profundamente en el alma 1 ó 2
+    veces por herida, preferiblemente en la noche durante varios días según
+    prescripción. Indicaciones: Antidepresivo de uso diario. Contra
+    indicaciones: Hipersensibilidad y lagrimeo excesivo. Precauciones y
+    advertencias: Se debe evitar el uso concomitante de otros productos que
+    contengan el componente de SORPRESA pues éste puede reducir su efectividad.
+    Dosis altas de cualquier SORPRESA probablemente conllevan un mayor riesgo de
+    estas reacciones, aunque los estudios clínicos controlados muestran que esto
+    no sucede en los casos donde el corazón lo necesitaba.
+  </Section>
+);
+
+export const WithColor = () => (
+  <Section color="teal-green">
+    Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos. Posología:
+    Aplicar un abrazo osado con un apretón fuerte profundamente en el alma 1 ó 2
+    veces por herida, preferiblemente en la noche durante varios días según
+    prescripción. Indicaciones: Antidepresivo de uso diario. Contra
+    indicaciones: Hipersensibilidad y lagrimeo excesivo. Precauciones y
+    advertencias: Se debe evitar el uso concomitante de otros productos que
+    contengan el componente de SORPRESA pues éste puede reducir su efectividad.
+    Dosis altas de cualquier SORPRESA probablemente conllevan un mayor riesgo de
+    estas reacciones, aunque los estudios clínicos controlados muestran que esto
+    no sucede en los casos donde el corazón lo necesitaba.
+  </Section>
+);
+
+export const WithContainer = () => (
+  <>
+    <Section color="teal-green" container="smesh">
+      <h3 className="h6">Smesh 1600px</h3>
+      Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos. Posología:
+      Posología: Aplicar un abrazo osado con un apretón fuerte profundamente en
+      el alma 1 ó 2 veces por herida
+    </Section>
+    <Section color="blue" container="smush">
+      <h3 className="h6">Smush 1032px</h3>
+      Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos. Posología:
+      Posología: Aplicar un abrazo osado con un apretón fuerte profundamente en
+      el alma 1 ó 2 veces por herida
+    </Section>
+    <Section color="lemon-green" container="smash">
+      <h3 className="h6">Smush 740px</h3>
+      Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos. Posología:
+      Posología: Aplicar un abrazo osado con un apretón fuerte profundamente en
+      el alma 1 ó 2 veces por herida
+    </Section>
+    <Section color="pink" container="smosh">
+      <h3 className="h6">Smush 440px</h3>
+      Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos. Posología:
+    </Section>
+    <Section color="yellow" container="smish">
+      <h3 className="h6">Smush 250px</h3>
+      Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos.
+    </Section>
+  </>
+);
+
+export const WithSubClassName = () => (
+  <Section color="pink" container="smash" subClassName="green aura">
+    <h3 className="h6">subClassName box</h3>
+    Cada abrazo de oso contiene: 1 dosis de amor por cada 2 brazos.
+  </Section>
+);
