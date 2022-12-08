fc530c219add30bb5013f3161cc8a77701b82db1
Pablo Orozco Montes <me@pablopvsky.com>
2022-12-08 11:07:08 -0500

    Update responsive fonts and clean code

---
+++ stories/headline.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Headlines = () => {
+    return <>
+        <h1>Headline 1</h1>
+        <h2>Headline 2</h2>
+        <h3>Headline 3</h3>
+        <h4>Headline 4</h4>
+        <h5>Headline 5</h5>
+        <h6>Headline 6</h6>
+    </>
+}
