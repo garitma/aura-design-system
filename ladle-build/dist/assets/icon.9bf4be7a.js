e6e1913903c17443d9b3071509df2b15977412dd
Pablo Orozco Montes <me@pablopvsky.com>
2022-12-08 21:46:44 -0500

    Update stories

---
+++ ladle-build/dist/assets/icon.9bf4be7a.js
@@ -0,0 +1,1 @@
+import{a as c}from"./index.9c217cd6.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
--- ladle-build/dist/assets/icon.b37a4e88.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./index.a7cfad2a.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
