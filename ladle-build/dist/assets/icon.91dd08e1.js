1c79f575849b31d87cf17fca69491d8543cbc5e9
Pablo Orozco Montes <me@pablopvsky.com>
2022-12-11 17:34:09 -0500

    Setup version 0.3.0

---
+++ ladle-build/dist/assets/icon.91dd08e1.js
@@ -0,0 +1,1 @@
+import{a as c}from"./index.3412066e.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
--- ladle-build/dist/assets/icon.9bf4be7a.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./index.9c217cd6.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
