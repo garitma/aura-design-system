15da9cf21935c76d01b095b9419061be728c15d5
Pablo Orozco Montes <me@pablopvsky.com>
2022-12-12 09:20:44 -0500

    remove custom code one signal

--- examples/with-one-signal/components/Footer.js
+++ examples/with-one-signal/components/Footer.js
@@ -12,32 +12,6 @@ const Footer = () => {
     OneSignal.init({
       appId: NEXT_PUBLIC_ONE_SIGNAL_APP_ID,
       safari_web_id: NEXT_PUBLIC_ONE_SIGNAL_SAFARI_WEB_ID,
-      notifyButton: {
-        enable: false,
-      },
-      allowLocalhostAsSecureOrigin: true,
-      // Your other init options here
-      promptOptions: {
-        customlink: {
-          enabled: true /* Required to use the Custom Link */,
-          style: "button" /* Has value of 'button' or 'link' */,
-          size: "medium" /* One of 'small', 'medium', or 'large' */,
-          color: {
-            button:
-              "var(--aura-accents-10)" /* Color of the button background if style = "button" */,
-            text: "#FFFFFF" /* Color of the prompt's text */,
-          },
-          text: {
-            subscribe:
-              "Subscribe to push notifications" /* Prompt's text when not subscribed */,
-            unsubscribe:
-              "Unsubscribe from push notifications" /* Prompt's text when subscribed */,
-            explanation:
-              "Get updates from all sorts of things that matter to you" /* Optional text appearing before the prompt button */,
-          },
-          unsubscribeEnabled: true /* Controls whether the prompt is visible after subscription */,
-        },
-      },
     }).then(() => {});
   }, []);
 
