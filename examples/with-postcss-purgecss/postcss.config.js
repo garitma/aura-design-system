5f8d8cf43d376a60c8f207c7faedeb18eee2af78
Pablo Orozco Montes <me@pablopvsky.com>
2023-01-02 21:05:51 -0500

    Purge css just in production

--- examples/with-postcss-purgecss/postcss.config.js
+++ examples/with-postcss-purgecss/postcss.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-  plugins: [
-    "postcss-flexbugs-fixes",
-    [
-      "postcss-preset-env",
-      {
-        autoprefixer: { flexbox: "no-2009" },
-        stage: 3,
-        features: { "custom-properties": false },
-      },
-    ],
-    [
-      "@fullhuman/postcss-purgecss",
-      {
-        content: [
-          "aura-design/**/*.{js,jsx,ts,tsx}",
-          "./components/**/*.{js,jsx,ts,tsx}",
-          "./pages/**/*.{js,jsx,ts,tsx}",
-        ],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body", "pad", "button-link"],
-      },
-    ],
-  ],
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: { flexbox: "no-2009" },
+              stage: 3,
+              features: { "custom-properties": false },
+            },
+          ],
+          [
+            "@fullhuman/postcss-purgecss",
+            {
+              content: [ 
+                "./components/**/*.{js,jsx,ts,tsx}",
+                "./pages/**/*.{js,jsx,ts,tsx}",
+              ],
+              defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+              safelist: ["html", "body", "pad", "button-link"],
+            },
+          ],
+        ]
+      : [],
 };
