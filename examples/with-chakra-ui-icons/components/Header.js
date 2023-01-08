00a0fb21a23c8413d82079485c09126333cb27f0
Pablo Orozco Montes <me@pablopvsky.com>
2023-01-07 21:59:39 -0500

    add example with chakra ui icons

---
+++ examples/with-chakra-ui-icons/components/Header.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+import Icon from "aura-design/icon";
+import Button from "aura-design/button";
+
+const Header = ({ text }) => {
+  return (
+    <header className="purple aura">
+      <div className="smush wall-pad">
+        <ul className="nav-list">
+          <li className="logo-circle">
+            <Link href="/" className="halo">
+              <Image
+                src="/logo.png"
+                width={48}
+                height={48}
+                priority={true}
+                alt="Logo Aura Design"
+              />
+            </Link>
+          </li>
+          <li className="mod">
+            <Button
+              mode="link"
+              href="https://github.com/garitma/aura-design-system/tree/canary/examples/next-base"
+              target="_blank"
+              rel="noopener"
+            >
+              <Icon sprite="github" />
+            </Button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div>{text && <h1 className="light centertxt mt0 mb0">{text}</h1>}</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ examples/with-chakra-ui-icons/pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import HeadContent from "@components/HeadContent";
+
+export default class extends Document {
+  render() {
+    return (
+      <Html lang="en-us">
+        <Head>
+          <meta charSet="UTF-8" />
+          <HeadContent />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
