c1a551da1d13afcbbbaada726347c827ec57bd1c
Pablo Orozco Montes <me@pablopvsky.com>
2023-01-09 18:29:41 -0500

    Refactor ladle folder

--- ladle-build/dist/assets/icon.4f6b4105.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./index.c4aa511a.js";const r=({sprite:s,className:n,...o})=>c("i",{className:["icon",s,n].join(" ").trim(),...o});export{r as I};
--- stories/icons.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import {UserIcon} from "@garitma/icons"
-
-export const IconUser = () => <UserIcon />;
-
-export const WithFontSize = () => <UserIcon className="h1" />;
