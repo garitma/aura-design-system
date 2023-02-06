53fdff5c9949c6731106fe3d32919ad39b5d5fe5
Pablo Orozco Montes <me@pablopvsky.com>
2023-02-05 22:16:30 -0500

    Config styles aura design npm

---
+++ packages/styles/prism.theme.aura-vc.js
@@ -0,0 +1,74 @@
+// Converted automatically using ./tools/themeFromVsCode
+var theme = {
+  plain: {
+    color: "#d4d4d4",
+    backgroundColor: "#0c122b",
+  },
+  styles: [
+    {
+      types: ["comment"],
+      style: {
+        fontWeight: "bold",
+      },
+    },
+    {
+      types: ["token", "tag", "attr-value"],
+      style: {
+        color: "#fbbc9f",
+      },
+    },
+    {
+      types: ["comment", "punctuation", "hexcode"],
+      style: {
+        color: "#CD9FFB",
+      },
+    },
+    {
+      types: [
+        "variable",
+        "keyword",
+        "tag",
+        "deleted",
+        "builtin",
+        "number",
+        "char",
+        "constant",
+      ],
+      style: {
+        color: "#9FDEFB",
+      },
+    },
+    {
+      types: ["function"],
+      style: {
+        color: "#FBEE9F",
+      },
+    },
+    {
+      types: ["string", "symbol", "inserted"],
+      style: {
+        color: "#FBBC9F",
+      },
+    },
+    {
+      types: ["changed"],
+      style: {
+        color: "#9FFBDE",
+      },
+    },
+    {
+      types: ["attr-name", "selector"],
+      style: {
+        color: "#C792EA",
+      },
+    },
+    {
+      types: ["regex"],
+      style: {
+        color: "#89DDFF",
+      },
+    },
+  ],
+};
+
+export default theme;
