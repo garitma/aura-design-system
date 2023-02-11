79fe92ce080bc908a07912c29d8631de30915ecd
Pablo Orozco Montes <me@pablopvsky.com>
2023-02-11 18:59:42 -0500

    Add fathom to playground

--- packages/playground/pages/_app.js
+++ packages/playground/pages/_app.js
@@ -1,9 +1,28 @@
-import Router from "next/router";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "aura-design/main.css";
 
 import "../public/style.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    Fathom.load(`USKQGVIM`, {
+      includedDomains: ["play.auradesignsystem.com"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
