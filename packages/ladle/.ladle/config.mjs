886e2a326e81a92fa80f2809eb24a181bad417ec
Pablo Orozco Montes <me@pablopvsky.com>
2023-02-11 20:05:37 -0500

    Add script

--- packages/ladle/.ladle/config.mjs
+++ packages/ladle/.ladle/config.mjs
@@ -1,6 +1,6 @@
 export default {
   appendToHead:
-    '<link rel="canonical" href="https://ladle.auradesignsystem.com/">',
+    '<link rel="canonical" href="https://ladle.auradesignsystem.com/"><script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="JULYXFEP" data-excluded-domains="localhost" defer></script>',
   outDir: "./build/dist",
   addons: {
     theme: {
