20e1bddd0a92e06841c1115097d6f587a2a46356
Pablo Orozco Montes <me@pablopvsky.com>
2023-02-11 19:13:40 -0500

    Fix clientside error

--- packages/playground/pages/_app.js
+++ packages/playground/pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import "aura-design/main.css";
 
 import "../public/style.css";
