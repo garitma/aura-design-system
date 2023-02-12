5688158235ca0df24bae9d77b07a9b04d095f294
Pablo Orozco Montes <me@pablopvsky.com>
2023-02-11 19:08:41 -0500

    Remove lock package

--- packages/ladle/.ladle/config.mjs
+++ packages/ladle/.ladle/config.mjs
@@ -1,6 +1,10 @@
 export default {
   appendToHead:
-    '<link rel="canonical" href="https://ladle.auradesignsystem.com/">',
+    `<link rel="canonical" href="https://ladle.auradesignsystem.com/">
+    <!-- Fathom - beautiful, simple website analytics -->
+    <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="JULYXFEP" defer></script>
+    <!-- / Fathom -->
+    `,
   outDir: "./build/dist",
   addons: {
     theme: {
