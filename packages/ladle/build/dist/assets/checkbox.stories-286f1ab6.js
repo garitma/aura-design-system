8a8d97a20f9b639e01d6252c2200b1fe074d2652
Pablo Orozco Montes <me@pablopvsky.com>
2023-02-26 17:22:46 -0500

    Build 0.4.30

--- packages/ladle/build/dist/assets/checkbox.stories-286f1ab6.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-830694c6.js";import{C as t}from"./checkbox-45274f5f.js";import"./input-c02fa55e.js";const m=()=>e(t,{label:"Accept terms and conditions."});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{m as Naked};
