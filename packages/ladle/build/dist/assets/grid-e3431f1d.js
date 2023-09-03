0cdaafd4da780bac131b80a6ea33bfba77363134
Pablo Orozco Montes <me@pablopvsky.com>
2023-09-03 18:24:23 -0500

    Update ladle

---
+++ packages/ladle/build/dist/assets/grid-e3431f1d.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-a87310cc.js";const c=({children:r,className:a,col:i,isFixed:e,...o})=>{const s=["aureole",a];return i&&s.push(i),e&&s.push("fixed"),t("div",{className:s.join(" ").trim(),...o,children:r})};export{c as G};
