e986016d966ee8944a573e220acff0b84eb50cb3
Pablo Orozco Montes <pablo@somosgentedigital.com>
2023-09-11 09:53:42 -0500

    Textarea component

---
+++ packages/ladle/stories/textarea.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Textarea from '../../components/textarea';
+
+export const WithPlaceholder = () => <Textarea placeholder="Placeholder" />;
+
+export const WithLabel = () => (
+    <Textarea placeholder="Placeholder and label" isLabelable />
+  );
