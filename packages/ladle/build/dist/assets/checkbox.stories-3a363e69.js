d0f706117c7142128167626e359f06237e651867
Pablo Orozco Montes <pablo@somosgentedigital.com>
2023-09-11 10:00:56 -0500

    Add textarea storie

--- packages/ladle/build/dist/assets/checkbox.stories-3a363e69.js
+++
@@ -1,9 +0,0 @@
-import { a as e } from "./index-84d4bf48.js";
-import { C as t } from "./checkbox-cce0589c.js";
-import "./input-ffabb90e.js";
-const m = () => e(t, { label: "Accept terms and conditions." });
-typeof window < "u" &&
-  window.document &&
-  window.document.createElement &&
-  document.documentElement.setAttribute("data-storyloaded", "");
-export { m as Naked };
