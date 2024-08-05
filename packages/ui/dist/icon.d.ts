18943d27cb708029e5765ec968b08971924d02a8
Pablo Orozco Montes <me@pablopvsky.com>
2024-08-04 21:42:49 -0500

    Remove dist

--- packages/ui/dist/icon.d.ts
+++
@@ -1,9 +0,0 @@
-import { SharedBasic } from "@aura-design/shate-types/global";
-export interface IconProps extends SharedBasic {
-    displayName?: string;
-    viewBox: string;
-    focusable?: boolean;
-}
-declare const Icon: ({ displayName, viewBox, children, focusable, className, }: IconProps) => JSX.Element;
-export default Icon;
-//# sourceMappingURL=icon.d.ts.map
