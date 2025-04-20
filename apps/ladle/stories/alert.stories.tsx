50d21517fd25f2e78d663b3226d64920c9889bdf
Pablo Orozco Montes <me@pablopvsky.com>
2025-04-20 11:48:23 -0500

    feat: add Alert component with customizable status and icons

--- apps/ladle/stories/alert.stories.tsx
+++ apps/ladle/stories/alert.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { Story } from "@ladle/react";
 
-import Alert from "@/components/ui/Alert";
+import Alert from "@/registry/default/ui/alert";
 
 export const Info: Story = () => (
   <Alert
--- apps/ladle/stories/button.stories.tsx
+++ apps/ladle/stories/button.stories.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import Button from "@aura-design/system/button";
 import type { Story } from "@ladle/react";
 
+import Button from "@/registry/default/ui/button";
+
 export const ButtonFill: Story = () => <Button label="Button fill" />;
-export const ButtonPill: Story = () => <Button mode="pill" label="Button Pill" />;
-export const ButtonLink: Story = () => <Button mode="link" label="Button Pill" />;
+export const ButtonPill: Story = () => (
+  <Button mode="pill" label="Button Pill" />
+);
+export const ButtonLink: Story = () => (
+  <Button mode="link" label="Button Pill" />
+);
 export const ButtonFluid: Story = () => <Button isFluid label="Button Fluid" />;
-export const ButtonDisabled: Story = () => <Button label="Disabled" isDisabled />;
+export const ButtonDisabled: Story = () => (
+  <Button label="Disabled" isDisabled />
+);
 export const ButtonWaiting: Story = () => (
-  <Button isLoading isLoadingText="Loading..." ></Button>
+  <Button isLoading isLoadingText="Loading..."></Button>
 );
 export const ButtonHref: Story = () => (
-  <Button mode="link" href="/"  label="I'm an href"></Button>
+  <Button mode="link" href="/" label="I'm an href"></Button>
 );
