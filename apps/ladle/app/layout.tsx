01fff169f2303e37594d37df3d4c7f3b5a99fe8e
Pablo Orozco Montes <me@pablopvsky.com>
2025-04-20 20:16:03 -0500

    refactor: remove n-progress.css and integrate styles into globals.css and registry.json

--- apps/ladle/app/layout.tsx
+++ apps/ladle/app/layout.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Link from "next/link";
 
-import "@/styles/globals.css"
+import "@/styles/globals.css";
 import Nprogress from "@/components/n-progress";
-
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
-        <Link href="/fo">Holi</Link>
         <div>{children}</div>
         <Nprogress />
       </body>
