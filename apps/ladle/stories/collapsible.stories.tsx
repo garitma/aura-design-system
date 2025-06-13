720dffa3ba1c84a8aedf7bb83678c43907ded694
Pablo Orozco Montes <me@pablopvsky.com>
2025-06-12 21:19:31 -0500

    feat: implement collapsible component with animations, enhance styling and structure in stories, and add new CSS for collapsible animations

--- apps/ladle/stories/collapsible.stories.tsx
+++ apps/ladle/stories/collapsible.stories.tsx
@@ -1,12 +1,13 @@
 import type { Story } from "@ladle/react";
 import { useState } from "react";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { RowSpacingIcon, Cross1Icon } from "@radix-ui/react-icons";
 
+import Button from "@/components/ui/Button";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "../components/ui/Collapsible";
+} from "@/components/ui/Collapsible";
 
 export const Default: Story = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,18 +16,29 @@ export const Default: Story = () => {
     <Collapsible
       open={isOpen}
       onOpenChange={setIsOpen}
+      className="max-w-md mx-auto"
     >
-      <CollapsibleTrigger asChild>
-        <button>
-          <span className="sr-only">Toggle</span>
-        </button>
-      </CollapsibleTrigger>
-
-      <div>@radix-ui/primitives</div>
-      <CollapsibleContent className="space-y-2 overflow-hidden text-sm data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
-        <div>@radix-ui/colors</div>
-        <div>@stitches/react</div>
-      </CollapsibleContent>
+      <div className="flex items-center justify-between">
+        <span>@peduarte starred 3 repositories</span>
+        <CollapsibleTrigger asChild>
+          <Button size="small">
+            {isOpen ? <Cross1Icon /> : <RowSpacingIcon />}
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+      <div className="space-y-0.5 mt-1">
+        <div className="border border-gray-a6 rounded-md p-1">
+          @radix-ui/primitives
+        </div>
+        <CollapsibleContent className="space-y-0.5">
+          <div className="border border-gray-a6 rounded-md p-1">
+            @radix-ui/colors
+          </div>
+          <div className="border border-gray-a6 rounded-md p-1">
+            @stitches/react
+          </div>
+        </CollapsibleContent>
+      </div>
     </Collapsible>
   );
 };
