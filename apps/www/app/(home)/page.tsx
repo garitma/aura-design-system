f0a5550ce3550777f975fa8d4e68a836379ef4d6
Pablo Orozco <pablo@somosgentedigital.com>
2025-11-13 11:44:31 -0500

    refactor: enhance global styles with additional CSS variables for improved theming; update homepage layout to include a Section component for better structure and styling

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 
 import { Button } from "@/components/ui/Button";
+import Section from "@/components/Section";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main>
-      <h1>Welcome to my imagination</h1>
-      <h3>Just another design system with my own taste.</h3>
-      <Link href="/docs">
-        <Button as="span">Get Started</Button>
-      </Link>
+    <main style={{ "--spacing": "13px" } as React.CSSProperties}>
+      <Section container="smush">
+        <div className="flex flex-col items-center justify-center text-center gap-1">
+          <h1 className="font-bold">Welcome to my imagination</h1>
+          <h3 className="h4">Just another design system with my own taste.</h3>
+          <Button asChild>
+            <Link href="/docs">Get Started</Link>
+          </Button>
+        </div>
+      </Section>
     </main>
   );
 }
