fab8cf28e6cbabcd1302bba473fdc87241c95e32
Pablo Orozco Montes <me@pablopvsky.com>
2025-11-18 21:27:16 -0500

    Update files

--- apps/www/components/ui/AspectRatio.tsx
+++
@@ -1,9 +0,0 @@
-import { AspectRatio as AspectRatioPradix } from "radix-ui";
-
-function AspectRatio({
-  ...props
-}: React.ComponentProps<typeof AspectRatioPradix.Root>) {
-  return <AspectRatioPradix.Root data-slot="aspect-ratio" {...props} />;
-}
-
-export { AspectRatio };
