c9afb0d46a6156f47726d88760a7b3883c6d7ab4
Pablo Orozco Montes <me@pablopvsky.com>
2025-11-22 12:36:34 -0500

    Move ladle to the registry

--- apps/ladle/app/api/tickets/route.ts
+++
@@ -1,7 +0,0 @@
-import { withValidation } from "@/utils/api-validation";
-import { createTicketSchema } from "@/schemas/ticketSchema";
-
-export const POST = withValidation(createTicketSchema, async (request) => {
-
-  return Response.json({ foo: "holi" });
-});
--- apps/ladle/components/ui/Input.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import { cn } from "@/utils/class-names";
-
-function Input({ className, ...props }: React.ComponentProps<"input">) {
-  return <input data-slot="input" className={cn(className)} {...props} />;
-}
-
-export { Input };
