972cfeb759dca605378bd872c63d62ed914b4068
“Pablo <me@pablopvsky.com>
2025-12-12 15:41:55 -0500

    refactor: Simplify RadioGroup component styles and improve accessibility with updated class names

--- packages/registry/registry/default/components/ui/RadioGroup.tsx
+++ packages/registry/registry/default/components/ui/RadioGroup.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-/**
- * @description A set of checkable buttons—known as radio buttons—where no more than one of the buttons can be checked at a time.
- */
 import * as React from "react";
 import { RadioGroup as RadioGroupPrimitive } from "radix-ui";
-import { DotFilledIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/utils/class-names";
 
@@ -16,7 +12,7 @@ function RadioGroup({
   return (
     <RadioGroupPrimitive.Root
       data-slot="radio-group"
-      className={cn("flex flex-col gap-1", className)}
+      className={cn(className, "cursor-pointer")}
       {...props}
     />
   );
@@ -30,17 +26,15 @@ function RadioGroupItem({
     <RadioGroupPrimitive.Item
       data-slot="radio-group-item"
       className={cn(
-        "border border-gray-a6 flex size-1.5 items-center justify-center rounded-full outline-none hover:bg-accent-2 cursor-pointer focus-visible:ring-2 focus-visible:ring-accent-7 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 transition-colors",
-        className
+        className,
+        "size-1.5 cursor-pointer rounded-full border border-gray-a6 hover:bg-accent-2"
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator
         data-slot="radio-group-indicator"
-        className="relative flex items-center justify-center"
-      >
-        <DotFilledIcon className="fill-accent-9 absolute top-1/2 left-1/2 size-0.5 -translate-x-1/2 -translate-y-1/2" />
-      </RadioGroupPrimitive.Indicator>
+        className="relative flex size-full items-center justify-center after:block after:size-0.5 after:rounded-full after:bg-accent-9"
+      />
     </RadioGroupPrimitive.Item>
   );
 }
